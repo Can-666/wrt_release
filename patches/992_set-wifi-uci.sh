f49d03c931dfbba5cbb4fa2cf8ab1ca1976cadd7
Can-666 <cacan666@163.com>
2026-02-26 23:27:14 +0800

    Update 992_set-wifi-uci.sh
    Signed-off-by: Can-666 <cacan666@163.com>

--- patches/992_set-wifi-uci.sh
+++ patches/992_set-wifi-uci.sh
@@ -35,8 +35,8 @@ EOF
 }
 
 jdc_ax1800_pro_wifi_cfg() {
-	configure_wifi 0 149 HE80 20 'JDC_AX1800PRO_5G' '12345678'
-	configure_wifi 1 1 HE20 20 'JDC_AX1800PRO' '12345678'
+	configure_wifi 0 149 HE80 20 'JD_5G' '12345678901'
+	configure_wifi 1 1 HE20 20 'JD_2.4G' '12345678901'
 }
 
 jdc_ax6600_wifi_cfg() {
